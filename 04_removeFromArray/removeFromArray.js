f725687accf3da7de7ea66228bf42d6f787f0467
diego-taboas <diegolitaboas@gmail.com>
2025-04-14 17:12:19 -0300

    Passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const argsSet = new Set(args);
+    return array.filter((num) => !argsSet.has(num));
 };
 
 // Do not edit below this line
