8aecb98cd64bf7b1adec8a5e43160239c4b7e7d8
diego-taboas <diegolitaboas@gmail.com>
2025-08-11 17:11:46 -0300

    Lesson 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+  value = Number(value);
+  let a = 0;
+  let b = 1;
 
+  if (value === 0) return a;
+  if (value === 1) return b;
+  if (value < 0) return("OOPS");
+  for (let i = 2; i <= value; i++) {
+  let next = a + b;
+  a = b;
+  b = next;
+  }
+  return b;
 };
 
 // Do not edit below this line
